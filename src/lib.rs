280982d3b728e5db4dc0d7bc906dbe6496c9d8c5
Paul Gusmano <155201655+pm-gusmano@users.noreply.github.com>
2025-08-26 17:36:21 -0400

    Revert "Extract time parsing logic Move time parsing logic to a separate module to separate concerns"
    This reverts commit ffdd1a9b6abb9009b5679fe8d57cdc331e6b5d61.

--- src/lib.rs
+++ src/lib.rs
@@ -1,15 +1,10 @@
 pub mod os_backend;
-
 pub mod blocking {
-    pub mod methods {
-        pub mod duration;
-    }
-    pub mod websites {
-        pub mod block_websites;
-    }
+    pub mod block_websites;
 }
-
-pub mod config {}
+pub mod config {
+    
+}
 
 pub mod cli {
     pub mod cli_runner;

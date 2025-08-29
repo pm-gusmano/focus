9874f442cc848c2bed141c72cb31466e1d1700c5
Paul Gusmano <paul.gusmano@outlook.com>
2025-08-29 15:53:33 -0400

    Set up basic unit test Import a vector of websites from a txt file

--- src/blocking/block.rs
+++ src/blocking/block.rs
@@ -1,28 +1,75 @@
-use std::time::Duration;
+use std::{fs, io, path::Path, time::Duration};
 
 struct Block {
+    name: String,
     block_list: SoftwareList,
     condition_list: BlockConditions,
     restriction_list: Restrictions,
     enabled: bool,
-    active: bool,
+    triggered: bool,
 }
 
 struct SoftwareList {
-    website_list: String,
-    application_list: String,
+    website_list: Option<Vec<String>>,
+    application_list: Option<Vec<String>>,
+}
+
+impl SoftwareList {
+    fn from_txt<P: AsRef<Path>>(path: P) -> io::Result<Self> {
+        let unsorted_software_list = fs::read_to_string(path)?;
+        return Err(io::Error::new(
+            io::ErrorKind::Other,
+            "Not implemented yet",
+        ));
+    }
 }
 
 struct BlockConditions {
     duration: Duration,
-    schedule: String,  // Implement with Jiff later
+    schedule: String, // Implement with Jiff later
     locations: String, // Implement with geo or some other location crate
-    // etc. (?)
+                      // etc. (?)
 }
 
 enum Restrictions {
     None,
     Password(String),
-    Location(String) // Implement with geo later
-    // etc.
+    Location(String), // Implement with geo later
+                      // etc.
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use tempfile::NamedTempFile;
+    use std::io::Write;
+
+    fn get_website_names() -> String {
+        return "google.com\n youtube.com \n wikipedia.org\n osu.edu".to_string();
+    }
+
+    #[test]
+    fn test_website_import_from_txt_file() {
+        // Arrange
+
+        // Write the generic websites to a txt file
+        let sites = get_website_names();
+        let mut temp_file = NamedTempFile::new().unwrap();
+        write!(temp_file, "{}", sites).unwrap();
+        let path = temp_file.path();
+
+        // Trim whitespace on each line of sites
+        let trimmed_sites: Vec<String> = sites
+            .lines()
+            .map(|line| line.trim().to_string())
+            .collect();
+
+        // Act
+        let software_list = SoftwareList::from_txt(path).unwrap();
+
+        // Assert
+        assert_eq!(software_list.website_list, Some(trimmed_sites));
+        assert_eq!(software_list.application_list, None)
+
+    }
 }
